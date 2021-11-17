5af5c863bb5169f5874ce97cb48c03426255d148
CCCCHHHH <94390042+Hao-Chen-NJUST@users.noreply.github.com>
2021-11-17 23:04:17 +0800

    Update rpc_one_img.py

--- rpc_one_img.py
+++ rpc_one_img.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 
 config_file = 'configs/rpc/faster_rcnn_r50_fpn_3x_rpc_protoS_srr_mll.py'
-checkpoint_file = 'result/faster_rcnn_r50_fpn_3x_rpc_protoS_ranking_mll/latest.pth'
+checkpoint_file = 'result/faster_rcnn_r50_fpn_3x_rpc_protoS_srr_mll/latest.pth'
 
 with open('/media/xxx/Data/RPC/instances_test2019.json', 'r') as f:
     data = json.load(f)
@@ -18,4 +18,4 @@ img = '/media/xxx/Data/RPC/test2019/20180824-15-44-39-474.jpg'
 img = mmcv.imread(img)
 result = inference_detector(model, img)
 
-model.show_result(img, result, out_file='faster_3x_rpc_protoS_ranking_mll/result.jpg', score_thr=0.35, thickness=4, font_size=13)
+model.show_result(img, result, out_file='faster_3x_rpc_protoS_srr_mll/result.jpg')
