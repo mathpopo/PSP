6c6451a6c7c527bb441713f69c8fd2a844cb880e
CCCCHHHH <94390042+Hao-Chen-NJUST@users.noreply.github.com>
2021-11-17 23:09:07 +0800

    Update rpc_one_img.py

--- rpc_one_img.py
+++ rpc_one_img.py
@@ -14,8 +14,8 @@ with open('/media/xxx/Data/RPC/instances_test2019.json', 'r') as f:
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
 
-img = '/media/xxx/Data/RPC/test2019/20180824-15-44-39-474.jpg'  
+img = '/media/xxx/Data/RPC/test2019/xxx.jpg'  
 img = mmcv.imread(img)
 result = inference_detector(model, img)
 
-model.show_result(img, result, out_file='faster_3x_rpc_protoS_srr_mll/result.jpg')
+model.show_result(img, result, out_file='faster_3x_rpc_protoS_srr_mll/xxx_result.jpg')
